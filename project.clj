e417545d128eabf38470cf256a86e1e28036859a
defpunk (aka David Oliver) <david.oliver@qwickr.com>
2016-06-10 08:27:09 +0100

    update to make lib play nicely with java

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject photon-client "0.1.1-SNAPSHOT"
+(defproject photon-client "0.1.3"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :aot :all
   :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [io.muoncore/muon-clojure "6.4-20160401135945"
